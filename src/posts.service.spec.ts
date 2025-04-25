a333196a163052a41ecd528bcb9eaef29a50cd6c
nastiasolo <ag.solodova@gmail.com>
2025-04-25 19:43:39 +0200

    tests added

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,32 @@
-import { Post, PostsService } from './posts.service';
+import { Post, PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
+  const post: Omit<Post, "id" | "date"> = {
+    text: "Mocked post",
   };
 
   beforeEach(async () => {
     postsService = new PostsService();
 
-    postsService.create({ text: 'Some pre-existing post' });
+    postsService.create({ text: "Some pre-existing post" });
   });
 
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
+  it("should add a new post", () => {
+    const createdPost = postsService.create(post);
+
+    expect(createdPost).toHaveProperty("id");
+    expect(createdPost).toHaveProperty("date");
+    expect(createdPost.text).toBe(post.text);
+
+    const found = postsService["posts"].find((p) => p.id === createdPost.id);
+    expect(found).toEqual(createdPost);
   });
 
-  it('should find a post', () => {
-    // реализуйте тест-кейс
+  it("should find a post", () => {
+    const createdPost = postsService.create(post);
+    const foundPost = postsService.find(createdPost.id);
+
+    expect(foundPost).toEqual(createdPost);
   });
 });
